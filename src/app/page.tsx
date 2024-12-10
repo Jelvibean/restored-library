7f0cd0dd9c83f737a4bb434008160364dc3ada03
Yelvy Buitrago <yelvyb@gmail.com>
2024-12-09 22:00:52 -0500

    yb: testing if cloning you would be able to get the project running and updated.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className={styles.wrapper}>
       <div className={styles.startbutton}>
-        <button onClick={handleOpenModal}>Open My Modal</button>
+        <button onClick={handleOpenModal}>Open Modal</button>
       </div>
       {isModalOpen && <Scenario onClose={handleCloseModal} />}
     </div>
